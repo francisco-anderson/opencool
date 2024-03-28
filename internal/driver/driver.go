26a6facc00c9c75426052cea0db34e97b2c55e2c
Francisco Anderson Rodrigues da Silva <contato@franciscoanderson.com>
2024-03-27 22:17:28 -0300

    fix: recreating usb context after lost previous connection

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -13,6 +13,8 @@ import (
 
 const VID = 0x34d3
 const PID = 0x1100
+const OUT_ENDPOINT = 0x01
+const IN_ENDPOINT = 0x82
 
 func StartDriver(configurations *config.Configurations) {
 
@@ -21,6 +23,17 @@ func StartDriver(configurations *config.Configurations) {
 		log.Fatalf("Unable to initialize driver: %v", err)
 	}
 
+	for {
+		err = driverLoop(device, configurations)
+		if err != nil {
+			log.Printf("Error while communicating with the device: %v", err)
+		}
+		time.Sleep(2 * time.Second)
+	}
+
+}
+
+func driverLoop(device *config.HhMonDeviceRegistered, configurations *config.Configurations) error {
 	// Initialize a new Context.
 	ctx := gousb.NewContext()
 	defer ctx.Close()
@@ -28,7 +41,9 @@ func StartDriver(configurations *config.Configurations) {
 	// Open any device with a given VID/PID using a convenience function.
 	dev, err := ctx.OpenDeviceWithVIDPID(VID, PID)
 	if err != nil {
-		log.Fatalf("Could not open a device: %v", err)
+		log.Println("Could not open a device")
+		return err
+
 	}
 	defer dev.Close()
 	dev.SetAutoDetach(true)
@@ -38,20 +53,23 @@ func StartDriver(configurations *config.Configurations) {
 	// config.
 	intf, done, err := dev.DefaultInterface()
 	if err != nil {
-		log.Fatalf("%s.DefaultInterface(): %v", dev, err)
+		log.Printf("%s.DefaultInterface()\n", dev)
+		return err
 	}
 	defer done()
 
 	// Open an OUT endpoint.
-	ep, err := intf.OutEndpoint(0x01)
+	ep, err := intf.OutEndpoint(OUT_ENDPOINT)
 	if err != nil {
-		log.Fatalf("%s.OutEndpoint(0x01): %v", intf, err)
+		log.Printf("%s.OutEndpoint(0x01)\n", intf)
+		return err
 	}
 
 	// Open an IN endpoint
-	in, err := intf.InEndpoint(0x82)
+	in, err := intf.InEndpoint(IN_ENDPOINT)
 	if err != nil {
-		log.Fatalf("%s.InEndpoint(0x82): %v", intf, err)
+		log.Printf("%s.InEndpoint(0x82)\n", intf)
+		return err
 	}
 
 	for {
@@ -64,12 +82,19 @@ func StartDriver(configurations *config.Configurations) {
 		// Write data to the USB device.
 		numBytes, err := ep.Write(data)
 		if numBytes != len(data) || err != nil {
-			log.Fatalf("%s.Write([5]): only %d bytes written, returned error is %v", ep, numBytes, err)
+			fmt.Printf("%s.Write([5]): only %d bytes written.\n", ep, numBytes)
+			return err
 		}
 
 		// Read data to the USB device.
 		res := make([]byte, 0x40)
 		numBytes, err = in.Read(res)
+
+		if numBytes != len(res) || err != nil {
+			fmt.Printf("%s.Read([5]): only %d bytes readed\n", ep, numBytes)
+			return err
+		}
+
 		stringRes := string(res)
 
 		if !strings.HasPrefix(stringRes, "OK(02)") {
@@ -79,5 +104,4 @@ func StartDriver(configurations *config.Configurations) {
 		time.Sleep(configurations.IntervalTime * time.Second)
 
 	}
-
 }
